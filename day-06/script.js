5427456b08f5cf9bac4627a06fc0fc7ae0751e5f
laasya1302 <laasyakantamreddy@gmail.com>
2026-01-29 14:28:04 +0530

    Intial commit

---
+++ day-06/script.js
@@ -0,0 +1,39 @@
+ let marks="84";
+ if(marks>=90){
+     console.log("grade A");
+ }
+ else if(marks>=70)
+ {
+     console.log("grade B");
+ }
+ else if(marks>=50)
+ {
+      console.log("grade C");
+ }
+ else if(marks>=36)
+{
+      conssole.log("grade D");
+ }
+ else{
+        console.log("fail");
+     }
+ for(let i=1;i<=10;i++){
+     console.log("5 X",i,"=",i*5);
+ } 
+ 
+ let student={
+    name:"laasya",
+     id:"2500040246",
+     branch:"ECE",
+     year:"1st year"
+ };
+ console.log("student Name:",student.name);
+ console.log("id:",student.id);
+ console.log("branch:",student.branch);
+ console.log("year:",student.year);
+ let num="20";
+ if(num%2==0){
+     console.log("num is even") }
+ else{
+     console.log("num is odd");
+ }
